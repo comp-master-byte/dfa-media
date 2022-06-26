84d4e9485091ea1535e4b3a73e5d1a1a437447b1
comp-master-byte <demoniac781@gmail.com>
2022-06-26 20:20:22 +0500

    Поправлено выделение активной страницы

--- src/UI/DfaLink/DfaLink.jsx
+++ src/UI/DfaLink/DfaLink.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import styles from "./DfaLink.module.scss";
 import { Link, useMatch } from "react-router-dom";
 
@@ -16,9 +16,9 @@ export const DfaLink = (props) => {
                     ? `${styles.activeLink} ${styles.link}`
                     : styles.link}
         >
+            <span className={styles.highlight}></span>
             <div className={styles.link__icon}>
                 {match ? <img src={activeIcon} alt="" /> : <img src={icon} alt="" />}
-
             </div>
             <div className={styles.link__text}>{textLink}</div>
         </Link>
